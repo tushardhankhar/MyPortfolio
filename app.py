7579457c7fb6996e42951be5295465b2df45b5f3
tushardhankhar <tushardhankhar98@gmail.com>
2021-08-11 17:48:36 +0530

    Resume added

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from re import template
-from flask import Flask , render_template
+from flask import Flask , render_template , send_file
 
 app = Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+
+@app.route('/resume-pdf/')
+def resume_pdf():
+	return send_file(r'C:\My Portfolio\static\tushar(tushardhankhar98@gmail.com).pdf', attachment_filename='tushar(tushardhankhar98@gmail.com).pdf')
+	
 
 if __name__ == '__main__':
     app.run(debug=True)
