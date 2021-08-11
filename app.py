a282563f1dee1da3131b55b54745bf1b80e3b96d
tushardhankhar <tushardhankhar98@gmail.com>
2021-08-11 12:28:43 +0530

    Base Web

---
+++ app.py
@@ -0,0 +1,11 @@
+from re import template
+from flask import Flask , render_template
+
+app = Flask(__name__,template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
